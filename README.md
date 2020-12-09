76b8a20b797f92a104dcf177915dcabb6b3cf69b
adxpillar <dsa.adeagbo@gmail.com>
2020-12-08 23:50:06 -0500

    complete basic CRUD

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Expense record tracker app 
-*Built from scratch: CLI to AWS*
-User should be able to:
+*Built from scratch: CLI to AWS*  
+User should be able to:  
 Add expense, list expenses, get expense, edit expense, and delete expense  
-100DaysOfMLCode # Python project suggested by *@jangia* : 
+100DaysOfMLCode # Python project suggested by *@jangia* :  
 > https://twitter.com/jangiacomelli/status/1331170946917384193
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def add_expense():
     """
-    adds an expense record using Entry class
+    # adds an expense record using Entry class
 
     args: none
     returns: none 
@@ -23,48 +23,91 @@ def add_expense():
 
     # write record to txt file
     with open('expense.txt','a') as f:
+        # write each record horizontally
         record = ','.join(map(str, [expense.title, expense.amount, expense.created_at, ' '.join(expense.tags)]))
         f.write(record + '\n')
         f.close()
 
+def get_expense(search_term):
+    """
+    # search expense record through title
+    args: title of expense
+    returns: record
 
-# get entry that's already in txt file 
+    """
+    with open('expense.txt', 'r') as f:
+        # read through each line in txt file to find search == result
+        lines = f.readlines()
+        found = None
+        for line in lines:
+            line = line.rstrip('\n').split(',')
+            if line[0].lower() == search_term:
+                found = line
+        return found
 
-# def get_expense(search_term):
-#     result = []
-#     with open('expense_report.txt') as f:
+def list_expense():
+    """
+    # list all expenses
+    args: none
+    return: none
+    """
+    with open('expense.txt', 'r') as f:
+        list_all_expenses = f.readlines()
+        for expense in list_all_expenses:
+            print(expense)
 
-#         if any (search_term in i for i in f.read()):
-#             result.append(i)
-#         else:
-#             print('error')
-#             raise ValueError("Try another search term")
+def update_expense(search_term):
+    """
+    # update an expense
+    args: search_term
+    return: none 
+    """
+    # get title of record to edit
+    record_to_edit = get_expense(search_term)
+    # delete the record 
+    delete_expense(search_term)
+    # instruct 
+    instruction = input('Enter "title" to edit title, "amount" to edit amount, and "tags" to edit tags: ').lower()
+    new_title = new_amount = new_tags = None 
+    if instruction == 'title':
+        while not isinstance(new_title, str):
+            new_title = input('Enter new title: ')
+            record_to_edit[0] = new_title
+    elif instruction == 'amount':
+        while not isinstance(amount, float):
+            new_amount = input('Update title: ')
+            record_to_edit[1] = new_amount
+    else:
+        while not isinstance(tags, str):
+            new_tags = input('Update tags: ').split()
+            record_to_edit[3] = new_tags
     
-#     print(result)
+    with open('expense.txt','a') as f:
+        f.write(','.join(record_to_edit) + '\n')
 
-# search_term = input('Enter search term: ')
-# search_op = get_expense(search_term)
-# print(search_op)
-
-
-#  List expenses 
-# def list_expense():
-#     # result = []
-#     with open('expense_report.txt') as f:
-#         result = f.read()
-    
-#     print(result)
-
-# list_expense()
-
-def update_expense():
-    pass
-
-def delete_expense():
-    pass
-
-# # delete entry 
-
+def delete_expense(search_term):
+    """
+    # open the file and get all lines. Then reopen the file in write mode 
+    # and write lines back, except for the line you want to delete:
+    args: title of record
+    return: none
+    """
+    result = []
+    # read all records except 'column' title
+    with open('expense.txt','r') as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            line = line.rstrip('\n').split(',')
+            # check if first record == target
+            if line[0].lower() == search_term:
+                continue
+            # else append all record to list 
+            result.append(line)
+    # write list into new txt file with 'columns'
+    with open('expense.txt', 'w') as f:
+        f.write('Title, Amount, Created_at,   Tags' + '\n')
+        for line in result:
+            f.write(','.join(line) + '\n')
 
 if __name__ == "__main__":
     while True:
